839ad32055c2436a98627994a2f4a3d1729cc82a
tinhnn <nguyennhutinh18@gmail.com>
2020-02-08 14:14:36 +0700

    master: add docs

---
+++ docs/precache-manifest.f284def78fbfb6b19cd36290f337bd79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e4fd85c6e96ee5be9c8",
+    "url": "/static/js/main.4e4fd85c.chunk.js"
+  },
+  {
+    "revision": "bc459066f36041f2ee07",
+    "url": "/static/js/1.bc459066.chunk.js"
+  },
+  {
+    "revision": "4e4fd85c6e96ee5be9c8",
+    "url": "/static/css/main.4ec34b54.chunk.css"
+  },
+  {
+    "revision": "bf3f98e0d633986e1032861132f08079",
+    "url": "/index.html"
+  }
+];
